ac739e6e455c5adee2045d5f20b9115563dc867b
Evan Summers <evan.summers@gmail.com>
2016-04-03 03:59:44 +0200

    readme

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,4 +1,6 @@
 
+set -e -u
+
 unset ns
 unset rediscli
 
@@ -6,5 +8,8 @@ ns='demo:ndeploy'
 rediscli='redis-cli -n 13'
 
   ns=$ns rediscli=$rediscli bin/ndeploy tclear13
-  ns=$ns rediscli=$rediscli bin/ndeploy pop 60 &
-  ns=$ns rediscli=$rediscli bin/ndeploy tdeploy
+  sleep 1
+  ns=$ns rediscli=$rediscli sh bin/ndeploy pop 10 &
+  sleep 2
+  deployDir=`ns=$ns rediscli=$rediscli sh bin/ndeploy tdeploy | tail -1`
+  echo deployDir $deployDir
